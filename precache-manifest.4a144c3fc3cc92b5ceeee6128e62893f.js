b3ab57b7f3810a3ace9df541d47925827abc6951
APE-LAIR\Admin <andie.ape@gmail.com>
2020-08-26 19:21:30 +0300

    Updates

---
+++ precache-manifest.4a144c3fc3cc92b5ceeee6128e62893f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d045bb72a1c116333b0c2be072acf003",
+    "url": "/react-sticker-app/index.html"
+  },
+  {
+    "revision": "6f241e955a196edf027e",
+    "url": "/react-sticker-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "32a7cfe486c70df8b4ef",
+    "url": "/react-sticker-app/static/css/main.76a86026.chunk.css"
+  },
+  {
+    "revision": "6f241e955a196edf027e",
+    "url": "/react-sticker-app/static/js/2.6a046bdb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-sticker-app/static/js/2.6a046bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a7cfe486c70df8b4ef",
+    "url": "/react-sticker-app/static/js/main.3577c2eb.chunk.js"
+  },
+  {
+    "revision": "462789f784a579252319",
+    "url": "/react-sticker-app/static/js/runtime-main.fa24029c.js"
+  }
+]);
